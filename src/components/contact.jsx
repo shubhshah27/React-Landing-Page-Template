569ca6557f7e34c2a24cf03fb93becc9f41bd7d5
shubhshah27 <152588566+shubhshah27@users.noreply.github.com>
2024-09-16 18:37:24 +0530

    basic website changes done

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -154,10 +154,10 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
+            &copy; 2024 Shubh Shah React Land Page Template. Design by{" "}
+            {/* <a href="http://www.templatewire.com" rel="nofollow">
               TemplateWire
-            </a>
+            </a> */}
           </p>
         </div>
       </div>
